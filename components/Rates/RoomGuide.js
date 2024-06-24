c56bc7e911d07c1392944742cf17e17b9b543bed
Jakub Plecháč <plechac.k@gmail.com>
2024-06-24 18:15:13 +0200

    🔧 responsive table

--- components/Rates/RoomGuide.js
+++ components/Rates/RoomGuide.js
@@ -12,7 +12,7 @@ const RoomGuide = () => {
   return (
     <div className="mb-8">
       {isExpanded && (
-        <div className="w-[50%] flex flex-col items-center m-auto relative">
+        <div className="w-full md:w-[50%] flex flex-col items-center m-auto relative">
           <div className="w-full overflow-x-auto shadow-md sm:rounded-lg mb-4">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
