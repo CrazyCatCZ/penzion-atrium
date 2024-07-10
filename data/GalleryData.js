a7a1fb4917fc0ee1a3472b3f303fec45e0211385
Jakub Plecháč <plechac.k@gmail.com>
2024-07-10 14:08:36 +0200

    added alt to gallery images

--- data/GalleryData.js
+++ data/GalleryData.js
@@ -37,81 +37,104 @@ const GalleryData = () => {
   const roomImages = [
     {
       src: Room1TImage,
+      alt: "Room1SImage",
     },
     {
       src: Room1SImage,
+      alt: "Room1TImage",
     },
     {
       src: Room2TImage,
+      alt: "Room2TImage",
     },
     {
       src: Room2SImage,
+      alt: "Room2SImage",
     },
     {
       src: Room3TImage,
+      alt: "Room3TImage",
     },
     {
       src: Room3TImage2,
+      alt: "Room3TImage2",
     },
     {
       src: Room4TImage,
+      alt: "Room4TImage",
     },
     {
       src: Room4SImage,
+      alt: "Room4SImage",
     },
     {
       src: Room5TImage,
+      alt: "Room5TImage",
     },
     {
       src: Room6TImage,
+      alt: "Room6TImage",
     },
     {
       src: Room7TImage,
+      alt: "Room7TImage",
     },
   ];
 
   const kitchenImages = [
     {
       src: BottomKitchenImage,
+      alt: "BottomKitchenImage",
     },
     {
       src: UpperKitchenImage,
+      alt: "UpperKitchenImage",
     },
   ];
 
   const outsideImages = [
     {
       src: FrontImage,
+      alt: "FrontImage",
     },
     {
       src: FrontMainImage,
+      alt: "FrontMainImage",
     },
     {
       src: PlaygroundImage,
+      alt: "PlaygroundImage",
     },
     {
       src: EntranceImage,
+      alt: "EntranceImage",
     },
   ];
 
   const toiletImages = [
     {
       src: WC1Image,
+      alt: "WC1Image",
     },
     {
       src: WC2Image,
+      alt: "WC2Image",
     },
     {
       src: WC3Image,
+      alt: "WC3Image",
     },
     {
       src: WC4Image,
+      alt: "WC4Image",
     },
     {
       src: WC5Image,
+      alt: "WC5Image",
     },
     {
       src: WC6Image,
+      alt: "WC6Image",
     },
   ];
 
